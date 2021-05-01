c273a8185a7ca5db7523dc7ea68357f3cab83352
Emlyn Price <emlyn.je.price@gmail.com>
2021-05-02 00:59:18 +0100

    Plot temperature averages

--- web_monitor/web_monitor/templog.py
+++ web_monitor/web_monitor/templog.py
@@ -1,7 +1,6 @@
 """Use flask to display temperature data from a temperature sensor and weather
 forecasts on a web page"""
 
-import sqlite3
 import datetime
 from bokeh.plotting import figure
 from bokeh.embed import json_item, components
@@ -16,6 +15,7 @@ from . import TimeForm
 
 bp = Blueprint('plots', __name__)
 
+
 def get_data_range(dt1, dt2):
     """Return all records from the database after the interval"""
 
@@ -29,6 +29,7 @@ def get_data_range(dt1, dt2):
 
     return rows
 
+
 def make_range_plot(dt1, dt2):
     """Plot the data in Bokeh"""
 
@@ -37,9 +38,15 @@ def make_range_plot(dt1, dt2):
 
     data = {'dates': dates,
             'cpu_temps': [r[1] for r in records],
-            'sensor_temps': [r[2] for r in records],
-            'current_duty_cycle': [r[3] for r in records],
-            'next_duty_cycle': [r[4] for r in records]
+            'cpu_temps_1_min': [r[2] for r in records],
+            'cpu_temps_5_min': [r[3] for r in records],
+            'cpu_temps_10_min': [r[4] for r in records],
+            'sensor_temps': [r[5] for r in records],
+            'sensor_temps_1_min': [r[6] for r in records],
+            'sensor_temps_5_min': [r[7] for r in records],
+            'sensor_temps_10_min': [r[8] for r in records],
+            'current_duty_cycle': [r[9] for r in records],
+            'next_duty_cycle': [r[10] for r in records]
             }
 
     source = ColumnDataSource(data=data)
@@ -63,37 +70,82 @@ def make_range_plot(dt1, dt2):
 
     cpu_temp_l = p.line(x='dates',
                         y='cpu_temps',
-                        color='#006ba4',
+                        color='#006BA4',
                         line_width=2,
                         source=source)
 
+    cpu_temp_1_min_l = p.line(x='dates',
+                              y='cpu_temps_1_min',
+                              color='#FF800E',
+                              line_width=2,
+                              source=source)
+
+    cpu_temp_5_min_l = p.line(x='dates',
+                              y='cpu_temps_5_min',
+                              color='#ABABAB',
+                              line_width=2,
+                              source=source)
+
+    cpu_temp_10_min_l = p.line(x='dates',
+                               y='cpu_temps_10_min',
+                               color='#595959',
+                               line_width=2,
+                               source=source)
+
     sensor_temp_l = p.line(x='dates',
                            y='sensor_temps',
-                           color='#ff800e',
+                           color='#5F9ED1',
                            line_width=2,
                            source=source)
 
+    sensor_temp_1_min_l = p.line(x='dates',
+                                 y='sensor_temps_1_min',
+                                 color='#C85200',
+                                 line_width=2,
+                                 source=source)
+
+    sensor_temp_5_min_l = p.line(x='dates',
+                                 y='sensor_temps_5_min',
+                                 color='#898989',
+                                 line_width=2,
+                                 source=source)
+
+    sensor_temp_10_min_l = p.line(x='dates',
+                                  y='sensor_temps_10_min',
+                                  color='#A2C8EC',
+                                  line_width=2,
+                                  source=source)
+
     curr_dc_l = p.line(x='dates',
                        y='current_duty_cycle',
-                       color='#ababab',
+                       color='#FFBC79',
                        line_width=2,
                        source=source,
                        y_range_name='duty_cycles')
 
     next_dc_l = p.line(x='dates',
                        y='next_duty_cycle',
-                       color='#595959',
+                       color='#CFCFCF',
                        line_width=2,
                        source=source,
                        y_range_name='duty_cycles')
 
     # The legend will be outside the plot, and so must be defined directly
-    leg = Legend(items=[
-        ("CPU Temperature", [cpu_temp_l]),
-        ("Sensor Temperature", [sensor_temp_l]),
-        ("Current Duty Cycle", [curr_dc_l]),
-        ("Next Duty Cycle", [next_dc_l]),
-    ], location=(5, 360))
+    leg = Legend(
+        items=[
+            ("CPU Temperature", [cpu_temp_l]),
+            ("CPU Temperature (1 min avg)", [cpu_temp_1_min_l]),
+            ("CPU Temperature (5 min avg)", [cpu_temp_5_min_l]),
+            ("CPU Temperature (10 min avg)", [cpu_temp_10_min_l]),
+            ("Sensor Temperature", [sensor_temp_l]),
+            ("Sensor Temperature (1 min avg)", [sensor_temp_1_min_l]),
+            ("Sensor Temperature (5 min avg)", [sensor_temp_5_min_l]),
+            ("Sensor Temperature (10 min avg)", [sensor_temp_10_min_l]),
+            ("Current Duty Cycle", [curr_dc_l]),
+            ("Next Duty Cycle", [next_dc_l]),
+        ],
+        location=(5, 360),
+        click_policy='hide')
 
     p.add_layout(LinearAxis(y_range_name='duty_cycles',
                             axis_label='Duty Cycle (%)',
@@ -114,7 +166,6 @@ def show_plot():
     now = datetime.datetime.now()
     datetime_1 = (now - datetime.timedelta(hours=72)).strftime('%Y-%m-%d %H:%M:%S')
     datetime_2 = now.strftime('%Y-%m-%d %H:%M:%S')
-
 
     if time_chooser.validate_on_submit():
         # if time_chooser.the_time.data != 'all':
